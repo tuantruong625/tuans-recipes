6a9c89d0cece03cefa45fe72932ce69de4174050
Tuan N. Truong <t.truong@vehikl.com>
2019-03-08 14:17:05 -0500

    Add vue native router

--- App.vue
+++ App.vue
@@ -1,44 +1,19 @@
 <template>
-  <view class="container">
-    <image
-          :style="{width: 300, height: 200}"
-          :source="require('./assets/undraw_eating_together_tjhx.png')"
-      />
-    <text class="text-color-primary">{{ title }}</text>
-    <button class="my-first-button" v-bind:title="buttonTitle" v-bind:on-press="handleBtnPress"/>
-    </view>
+  <app-navigation></app-navigation>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      title: 'Tuans Recipes',
-      buttonTitle: 'Click Me'
-
-    }
+import { StackNavigator } from "vue-native-router";
+import SplashScreen from './src/screens/SplashScreen.vue';
+const AppNavigation = StackNavigator(
+  {
+    Home: SplashScreen,
   },
-  methods: {
-    handleBtnPress() {
-      alert('Hi');
-    }
+  {
+    initialRouteName: 'Home',
   }
+);
+export default {
+  components: { AppNavigation },
 }
 </script>
-
-
-<style>
-.container {
-  background-color: white;
-  align-items: center;
-  justify-content: center;
-  flex: 1;
-}
-.text-color-primary {
-  font-size: 25;
-  font-weight: 600;
-  letter-spacing: 1.5;
-  margin-top: 2;
-  color: #1c7ed6;
-}
-</style>
---
+++ src/screens/SplashScreen.vue
@@ -0,0 +1,43 @@
+<template>
+  <view class="container">
+    <image
+          :style="{width: 300, height: 200}"
+          :source="require('../../assets/undraw_eating_together_tjhx.png')"
+      />
+    <text class="text-color-primary">{{ title }}</text>
+    <button class="my-first-button" v-bind:title="buttonTitle" v-bind:on-press="handleBtnPress"/>
+    </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Tuans Recipes',
+      buttonTitle: 'Click Me'
+    }
+  },
+  methods: {
+    handleBtnPress() {
+      alert('Hi');
+    }
+  }
+}
+</script>
+
+
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  font-size: 25;
+  font-weight: 600;
+  letter-spacing: 1.5;
+  margin-top: 2;
+  color: #1c7ed6;
+}
+</style>
